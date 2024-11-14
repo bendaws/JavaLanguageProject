e7cf141275b0e1c20942b84370d46a1bf15b416d
Ben <ben@bendaws.net>
2024-11-14 13:49:32 -0600

    input stuff

---
+++ scans.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Scanning {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.printf("You said %s?\n", input); 
+	}
+}
